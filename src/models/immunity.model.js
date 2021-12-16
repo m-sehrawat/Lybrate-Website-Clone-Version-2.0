62250f7abdf4e928188172aa87a2f9d863406e4b
maheshvaitla <maheshvaitla@gmail.com>
2021-12-16 12:24:46 +0530

    code added

---
+++ src/models/immunity.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const immunitySchema = new mongoose.Schema({
+    img:[{type:String,required:true}],
+    companyname:{type:String,required:true},
+    name:{type:String,required:true},
+    newprice:{type:Number,required:true},
+    oldprice:{type:Number,required:true},
+    off:{type:String,required:true},
+    
+},{
+versionKey:false,
+timestamps:true
+})
+
+module.exports=mongoose.model("immunity", immunitySchema);
---
+++ src/models/vitamins.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const vitaminsSchema = new mongoose.Schema({
+    img:[{type:String,required:true}],
+    companyname:{type:String,required:true},
+    name:{type:String,required:true},
+    newprice:{type:Number,required:true},
+    oldprice:{type:Number,required:true},
+    off:{type:String,required:true},
+    
+},{
+versionKey:false,
+timestamps:true
+})
+
+module.exports=mongoose.model("vitamins", vitaminsSchema);
